6e1ce300640972fe7990f9ab21c886d26afb5aff
Victor Meunier <mreliptik@pop-os.localdomain>
2019-11-09 13:09:43 +0100

    change game path

--- game/Dead_screen.gd
+++ game/Dead_screen.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_restart"):
-		get_tree().change_scene("res://game.tscn")
+		get_tree().change_scene("res://game/level_1/scenes/game.tscn")
