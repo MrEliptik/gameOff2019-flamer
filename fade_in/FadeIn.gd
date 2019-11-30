0cbfd7318d0e4fd4ca1eb34c5e2d57061aaba55d
MrEliptik <victormeunier.dev@gmail.com>
2019-12-01 00:50:21 +0100

    remove unused and/or rename

--- fade_in/FadeIn.gd
+++
@@ -1,9 +0,0 @@
-extends ColorRect
-
-signal fade_finished
-
-func fade_in():
-	$AnimationPlayer.play("FadeIn")
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	emit_signal("fade_finished")
--- titlescreen/scripts/ReturnToTitle.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-
-
-func _on_Button_pressed():
-	get_tree().change_scene("res://titlescreen/scenes/titlescreen.tscn")
