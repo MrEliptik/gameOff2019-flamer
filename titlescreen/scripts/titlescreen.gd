2b1b6dded6adddb70214dc5bffbba528a7d8a50b
MrEliptik <victormeunier.dev@gmail.com>
2019-11-30 01:47:58 +0100

    add animation

--- titlescreen/scripts/titlescreen.gd
+++ titlescreen/scripts/titlescreen.gd
@@ -10,6 +10,8 @@ const JOY_DEADZONE = 0.2
 var y_val = 0
 var index_select = 0
 
+var leaving = false
+
 enum DIRECTION{
 	UP,
 	DOWN	
@@ -59,13 +61,20 @@ func _input(e):
 		btns[index_select].grab_focus()
 		
 func _physics_process(delta):
-	
+	if leaving: return
 	if $Menu/CenterRow/Buttons/PlayButton.is_hovered() == true:
 		$Menu/CenterRow/Buttons/PlayButton.grab_focus()
+		# Flame selector
 		$Selector1.visible = true
 		$Selector2.visible = false
 		$Selector3.visible = false
 		$Selector4.visible = false
+		
+		# Fire burning
+		$Letter_burn1.visible = true
+		$Letter_burn2.visible = false
+		$Letter_burn3.visible = false
+		$Letter_burn4.visible = false
 		
 		scaleLabel($Menu/CenterRow/Buttons/PlayButton/Label, SCALE_FACTOR_X, SCALE_FACTOR_Y)
 		scaleLabel($Menu/CenterRow/Buttons/TutorialButton/Label, 1.0, 1.0)
@@ -78,6 +87,11 @@ func _physics_process(delta):
 		$Selector3.visible = false
 		$Selector4.visible = false
 		
+		$Letter_burn1.visible = false
+		$Letter_burn2.visible = true
+		$Letter_burn3.visible = false
+		$Letter_burn4.visible = false
+		
 		scaleLabel($Menu/CenterRow/Buttons/PlayButton/Label, 1.0, 1.0)
 		scaleLabel($Menu/CenterRow/Buttons/TutorialButton/Label, SCALE_FACTOR_X, SCALE_FACTOR_Y)
 		scaleLabel($Menu/CenterRow/Buttons/OptionsButton/Label, 1.0, 1.0)
@@ -88,6 +102,11 @@ func _physics_process(delta):
 		$Selector2.visible = false
 		$Selector3.visible = true
 		$Selector4.visible = false
+		
+		$Letter_burn1.visible = false
+		$Letter_burn2.visible = false
+		$Letter_burn3.visible = true
+		$Letter_burn4.visible = false
 		
 		scaleLabel($Menu/CenterRow/Buttons/PlayButton/Label, 1.0, 1.0)
 		scaleLabel($Menu/CenterRow/Buttons/TutorialButton/Label, 1.0, 1.0)
@@ -100,12 +119,28 @@ func _physics_process(delta):
 		$Selector3.visible = false
 		$Selector4.visible = true
 		
+		$Letter_burn1.visible = false
+		$Letter_burn2.visible = false
+		$Letter_burn3.visible = false
+		$Letter_burn4.visible = true
+		
 		scaleLabel($Menu/CenterRow/Buttons/PlayButton/Label, 1.0, 1.0)
 		scaleLabel($Menu/CenterRow/Buttons/TutorialButton/Label, 1.0, 1.0)
 		scaleLabel($Menu/CenterRow/Buttons/OptionsButton/Label, 1.0, 1.0)
 		scaleLabel($Menu/CenterRow/Buttons/ExitButton/Label, SCALE_FACTOR_X, SCALE_FACTOR_Y)
 		
 func _on_button_pressed(scene_to_load):
+	leaving = true
+	if $Menu/CenterRow/Buttons/PlayButton.is_hovered() == true:	
+		# Fire burning
+		$Letter_burn1.play("burn")
+	elif $Menu/CenterRow/Buttons/TutorialButton.is_hovered() == true:
+		$Letter_burn2.play("burn")
+	elif $Menu/CenterRow/Buttons/OptionsButton.is_hovered() == true:
+		$Letter_burn3.play("burn")
+	elif $Menu/CenterRow/Buttons/ExitButton.is_hovered() == true:
+		$Letter_burn4.play("burn")
+	
 	if scene_to_load == "-1": 
 		get_tree().quit()
 	else:	
