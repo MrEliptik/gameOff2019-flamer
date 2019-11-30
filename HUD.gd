17fb63f7476861f377ee8371d9bb4d63f2ae5f7a
MrEliptik <victormeunier.dev@gmail.com>
2019-11-30 01:46:36 +0100

    update hud with stats

--- HUD.gd
+++ HUD.gd
@@ -3,5 +3,8 @@ extends Control
 func updateScore(score):
 	$HBoxContainer/HBoxContainer/Value.text = str(score)
 	
-func updateFPS(fps):
-	$HBoxContainer/HBoxContainer2/Value.text = str(fps)
+func updateStats(stats):
+	$HBoxContainer/HBoxContainer1/Value.text = str(stats["score"])
+	$HBoxContainer/HBoxContainer2/Value.text = str(stats["air_time"])
+	$HBoxContainer/HBoxContainer3/Value.text = str(stats["perfect_jumps"])
+	$HBoxContainer/HBoxContainer4/Value.text = str(stats["platforms_hit"])
