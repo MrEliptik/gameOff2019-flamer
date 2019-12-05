003cb7cc875850b03e91bf944665d208a44027b4
MrEliptik <victormeunier.dev@gmail.com>
2019-12-05 22:22:08 +0100

    cast score to int

--- player/HUD.gd
+++ player/HUD.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func updateScore(score):
-	$HBoxContainer/HBoxContainer/Value.text = str(score)
+	$HBoxContainer/HBoxContainer/Value.text = str(int(score))
 	
 func updateStats(stats):
-	$HBoxContainer/HBoxContainer1/Value.text = str(stats["score"])
+	$HBoxContainer/HBoxContainer1/Value.text = str(int(stats["score"]))
 	$HBoxContainer/HBoxContainer2/Value.text = str(stats["air_time"])
 	$HBoxContainer/HBoxContainer3/Value.text = str(stats["perfect_jumps"])
 	$HBoxContainer/HBoxContainer4/Value.text = str(stats["platforms_hit"])
