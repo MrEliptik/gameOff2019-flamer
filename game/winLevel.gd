84f1885bbb0d0a9937c240d6cccf477b9af56f97
MrEliptik <victormeunier.dev@gmail.com>
2019-12-08 16:47:22 +0100

    update font size color and placement

--- game/winLevel.gd
+++ game/winLevel.gd
@@ -11,12 +11,13 @@ func _process(delta):
 		get_tree().change_scene(nextLevel)
 
 func setStats(stats):
-	$VBoxContainer2/HBoxContainer1/Value.text = str(stats["avg_air_time"])
-	$VBoxContainer2/HBoxContainer2/Value.text = str(stats["max_air_time"])
-	$VBoxContainer2/HBoxContainer3/Value.text = str(stats["jump_count"])
-	$VBoxContainer2/HBoxContainer4/Value.text = str(stats["perfect_jumps"])
-	$VBoxContainer2/HBoxContainer5/Value.text = str(stats["platforms_hit"])
-	$VBoxContainer2/HBoxContainer6/Value.text = str(stats["score"])
+	$VBoxContainer2/HBoxContainer0/HBoxContainer/Value.text = str(int(stats["level_time"]/1000))
+	$VBoxContainer2/HBoxContainer1/HBoxContainer/Value.text = str(stats["avg_air_time"])
+	$VBoxContainer2/HBoxContainer2/HBoxContainer/Value.text = str(stats["max_air_time"])
+	$VBoxContainer2/HBoxContainer3/HBoxContainer/Value.text = str(stats["jump_count"])
+	$VBoxContainer2/HBoxContainer4/HBoxContainer/Value.text = str(stats["perfect_jumps"])
+	$VBoxContainer2/HBoxContainer5/HBoxContainer/Value.text = str(stats["platforms_hit"])
+	$VBoxContainer2/HBoxContainer6/Value.text = str(int(stats["score"]))
 
 func _on_BlinkTimer_timeout():
 	if $CenterContainer/Label.is_visible():
