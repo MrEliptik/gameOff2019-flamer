d5c574e95343016abfa44ae31867975736f8a1c8
Victor <victormeunier.dev@gmail.com>
2019-12-14 19:19:25 +0100

    remove unused

--- game/level_2/scenes/level_2.gd
+++ game/level_2/scenes/level_2.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
 
-func _ready():
-	$MovingSaws/MovingCircularSaw/AnimationPlayer.set_autoplay("Up_and_down")
-
 func _on_Powerup_body_entered(body):
 	if body is PhysicsBody2D:
 		$Powerup.set_deferred("monitoring", false)                             
